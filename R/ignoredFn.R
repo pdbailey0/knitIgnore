cba00157fb21fdf7c2e98fcf630e9330f9ceff30
unknown <pbailey@air.org>
2017-03-23 00:00:12 -0500

    make it work

--- R/ignoredFn.R
+++ R/ignoredFn.R
@@ -1,5 +1,9 @@
 #' @title A function that should not appear in the package
 #'
+#' @description Blank
+#'
+#' @param x a variable
+#'
 #' @export
 ignoreFn <- function(x) {
   cat("works!")
--- R/kept.R
+++ R/kept.R
@@ -1,5 +1,9 @@
 #' @title A function that appears in the package
 #'
+#' @description Blank
+#'
+#' @param x a variable
+#'
 #' @export
 kept <- function(x) {
   cat("works!")
